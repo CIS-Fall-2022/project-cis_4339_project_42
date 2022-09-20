2533ec17559562ba9a327d7129d6eac520d0b6a3
jsasser100 <70601078+jsasser100@users.noreply.github.com>
2022-09-20 18:13:44 -0500

    Comment the Org ID get all

--- backend/routes/primaryData.js
+++ backend/routes/primaryData.js
@@ -18,6 +18,22 @@ router.get("/", (req, res, next) => {
     ).sort({ 'updatedAt': -1 }).limit(10);
 });
 
+
+//GET all entries For Organization
+//router.get("/", (req, res, next) => { 
+    //primarydata.find( 
+        //(error, data) => {
+            //if (error) {
+                //return next(error);
+            //} else {
+                //res.json(data);
+            /*}
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
+});
+*/
+
+
 //GET single entry by ID
 router.get("/id/:id", (req, res, next) => {
     primarydata.find( 
