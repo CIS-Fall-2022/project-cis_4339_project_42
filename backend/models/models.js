8ba895f4e87548189acbe9e32c20956c98e3e13f
Michael Stokes <mtstoke2@cougarnet.uh.edu>
2022-09-21 11:49:10 -0500

    Added the first iteration of organization schema

--- backend/models/models.js
+++ backend/models/models.js
@@ -2,6 +2,18 @@ const uuid = require('uuid');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//collection for the Organizations
+let organizationSchema = new Schema({
+    _id: { type: String, default: uuid.v1 },
+    organizationName: {
+        type: String,
+        require: true
+    }
+}, {
+    collection: 'organizationData',
+    timestamps: true
+});
+
 //collection for intakeData
 let primaryDataSchema = new Schema({
     _id: { type: String, default: uuid.v1 },
