19c55ae9455d7be9391e1e42b04fe203d0d13c06
jsasser100 <70601078+jsasser100@users.noreply.github.com>
2022-10-27 11:42:55 -0500

    Getting Back on track

--- frontend/src/components/eventDetails.vue
+++ frontend/src/components/eventDetails.vue
@@ -286,7 +286,8 @@ export default {
   beforeMount() {
     axios
       .get(
-        import.meta.env.VITE_ROOT_API + `/eventdata/${this.$route.params.oid}` //able to get Clients information to stay when Updating
+        //import.meta.env.VITE_ROOT_API + `/eventdata/${this.$route.params.oid}` //able to get Clients information to stay when Updating
+        import.meta.env.VITE_ROOT_API + `/eventdata/${this.$route.params.id}`
       )
       .then((resp) => {
         let data = resp.data[0];
