e480e8167f582463933a93e92402775aaf6d2fc2
jsasser100 <70601078+jsasser100@users.noreply.github.com>
2022-10-27 11:10:01 -0500

    Getting back to what I had

--- frontend/src/components/updateClient.vue
+++ frontend/src/components/updateClient.vue
@@ -49,7 +49,7 @@ export default {
     axios
       .get(
         import.meta.env.VITE_ROOT_API +
-          `/primarydata/id/${this.$route.params.id}`
+          `/primarydata/${this.$route.params.oid}/id/${this.$route.params.id}`
       )
       .then((resp) => {
         let data = resp.data[0];
