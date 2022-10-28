3c5d559456326310d962481205d2b4d4418c8d48
jsasser100 <70601078+jsasser100@users.noreply.github.com>
2022-10-28 17:32:08 -0500

    Able to make OID invisible in find pages

--- frontend/src/components/findClient.vue
+++ frontend/src/components/findClient.vue
@@ -2,7 +2,7 @@
   <main>
     <div>
       <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Find Client</h1>
-      <h2> VITE_OID: {{orgID}}</h2>
+      <h2 class="invisible"> VITE_OID: {{orgID}}</h2>
     </div>
     <div class="px-10 pt-20">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
