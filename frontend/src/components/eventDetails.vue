91fd3fea2a85720d58dc72d8ce202c9574108396
jsasser100 <70601078+jsasser100@users.noreply.github.com>
2022-11-15 17:18:25 -0600

    Revert "Revert "Was able to get""
    This reverts commit 8a6bf98515390a1220543b3653ae3845fc09c565.

--- frontend/src/components/eventDetails.vue
+++ frontend/src/components/eventDetails.vue
@@ -128,13 +128,20 @@
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Address Line 1</span>
-              <!--span style="color:#ff0000">*</span-->
+              <span style="color:#ff0000">*</span>
               <input
                 type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder
                 v-model="event.address.line1"
               />
+              <span class="text-black" v-if="v$.event.address.line1.$error">
+                <p
+                  class="text-red-700"
+                  v-for="error of v$.event.address.line1.$errors"
+                  :key="error.$uid"
+                >{{ error.$message }}!</p>
+              </span>
             </label>
           </div>
           <!-- form field -->
@@ -153,13 +160,20 @@
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">City</span>
-              <!--span style="color:#ff0000">*</span-->
+              <span style="color:#ff0000">*</span>
               <input
                 type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder
                 v-model="event.address.city"
               />
+              <span class="text-black" v-if="v$.event.address.city.$error">
+                <p
+                  class="text-red-700"
+                  v-for="error of v$.event.address.city.$errors"
+                  :key="error.$uid"
+                >{{ error.$message }}!</p>
+              </span>
             </label>
           </div>
           <div></div>
@@ -179,12 +193,20 @@
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Zip Code</span>
+              <span style="color:#ff0000">*</span>
               <input
                 type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder
                 v-model="event.address.zip"
               />
+              <span class="text-black" v-if="v$.event.address.zip.$error">
+                <p
+                  class="text-red-700"
+                  v-for="error of v$.event.address.zip.$errors"
+                  :key="error.$uid"
+                >{{ error.$message }}!</p>
+              </span>
             </label>
           </div>
         </div>
@@ -352,8 +374,13 @@ export default {
   validations() {
     return {
       event: {
-        eventName: { required, alpha },
+        eventName: { required },
         date: { required },
+        address: {
+          line1: { required },
+          city: { required, alpha },
+          zip: { required, numeric }
+        },
       },
     };
   },
